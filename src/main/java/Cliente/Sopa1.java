1543e6735303dee0733b2f9c38adfcb874126b10
LuisEG98 <61921828+LuisEG98@users.noreply.github.com>
2020-03-07 19:46:56 -0600

    Validaciones
    validaciones para que las palabras no se salgan del tablero

--- src/main/java/Cliente/Sopa1.java
+++ src/main/java/Cliente/Sopa1.java
@@ -15,56 +15,83 @@ public class Sopa1 {
     public static void horizontalNormal(String tablero[][],String palabra){
         Random auxf=new Random();
         Random auxc=new Random();
-        int fila=(int)(auxf.nextDouble()*14);
-        int col=(int)(auxc.nextDouble()*10);
+        int fila=(int)(auxf.nextDouble()*(tablero.length));
+        int col=(int)(auxc.nextDouble()*((tablero.length)-(palabra.length())));
         for(int i=0; i<palabra.length();i++){
-            if(tablero[fila][col+i]==null){
                 tablero[fila][col+i]=""+palabra.charAt(i);
-            }else{
-                horizontalNormal(tablero,palabra);
-            }
         }
     }
-    public static void horizontalInvertida(String tablero[][],String palabra,int fila,int colum){
+    public static void horizontalInvertida(String tablero[][],String palabra){
         int a=0;
+        Random auxf=new Random();
+        Random auxc=new Random();
+        int fila=(int)(auxf.nextDouble()*(tablero.length));
+        int col=(int)(auxc.nextDouble()*((tablero.length)-(palabra.length())));
         for(int i=palabra.length()-1; i>=0;i--){
-            tablero[fila][colum+a]=""+palabra.charAt(i);
+            tablero[fila][col+a]=""+palabra.charAt(i);
             a++;
         }
     }
     public static void verticalNormal(String tablero[][],String palabra){
         Random auxf=new Random();
         Random auxc=new Random();
-        int fila=(int)(auxf.nextDouble()*14);
-        int col=(int)(auxc.nextDouble()*10);
+        int fila=(int)(auxf.nextDouble()*((tablero.length)-(palabra.length())));
+        int col=(int)(auxc.nextDouble()*tablero.length);
         for(int i=0; i<palabra.length();i++){
-            if(tablero[fila+i][col]==null){
                 tablero[fila+i][col]=""+palabra.charAt(i);
-            }else{
-                horizontalNormal(tablero,palabra);
-            }
         }
     }
-    public static void diagonalIzqAbajo(String tablero[][],String palabra,int fila,int colum){
-        for(int i=0; i<palabra.length();i++){
-            tablero[fila+i][colum+i]=""+palabra.charAt(i);
-        }
-    }
-    public static int[] GenerarCoordenadas(String tablero[][],String palabra){
-        int[] coordenadas = new int[2];
+    public static void verticalInvertida(String tablero[][],String palabra){
         Random auxf=new Random();
         Random auxc=new Random();
-        do{
-            coordenadas[1] = (int)(auxf.nextDouble()*14);
-            coordenadas[2] = (int)(auxc.nextDouble()*10);
-        }while(verificar(palabra,tablero,coordenadas[1],coordenadas[2]));
-        return coordenadas;
+        int a=0;
+        int fila=(int)(auxf.nextDouble()*((tablero.length)-(palabra.length())));
+        int col=(int)(auxc.nextDouble()*tablero.length);
+        for(int i=palabra.length()-1; i>=0;i--){
+            tablero[fila+a][col]=""+palabra.charAt(i);
+            a++;
+        }
     }
-    public static boolean verificar(String palabra, String tablero[][], int fila, int colum){
-    
-        return true;
+    public static void diagonalIzqAbajo(String tablero[][],String palabra){
+        Random auxf=new Random();
+        Random auxc=new Random();
+        int fila=(int)(auxf.nextDouble()*((tablero.length)-(palabra.length())));
+        int col=(int)(auxc.nextDouble()*((tablero.length)-(palabra.length())));
+        for(int i=0; i<palabra.length();i++){
+            tablero[fila+i][col+i]=""+palabra.charAt(i);
+        }
     }
-    
+    public static void diagonalAbajoIzq(String tablero[][],String palabra){
+        Random auxf=new Random();
+        Random auxc=new Random();
+        int a=0;
+        int fila=(int)(auxf.nextDouble()*((tablero.length)-(palabra.length())));
+        int col=(int)(auxc.nextDouble()*((tablero.length)-(palabra.length())));
+        for(int i=palabra.length()-1; i>=0;i--){
+            tablero[fila+a][col+a]=""+palabra.charAt(i);
+            a++;
+        }
+    }
+    public static void diagonalAbajoDer(String tablero[][],String palabra){
+        Random auxf=new Random();
+        Random auxc=new Random();
+        int fila=(int)(auxf.nextDouble()*((tablero.length)-(palabra.length()))+palabra.length());
+        int col=(int)(auxc.nextDouble()*((tablero.length)-(palabra.length())));
+        for(int i=0; i<palabra.length();i++){
+            tablero[fila-i][col+i]=""+palabra.charAt(i);
+        }
+    }
+    public static void diagonalDerAbajo(String tablero[][],String palabra){
+        Random auxf=new Random();
+        Random auxc=new Random();
+        int a=0;
+        int fila=(int)(auxf.nextDouble()*((tablero.length)-(palabra.length()))+palabra.length());
+        int col=(int)(auxc.nextDouble()*((tablero.length)-(palabra.length())));
+        for(int i=palabra.length()-1; i>=0;i--){
+            tablero[fila-a][col+a]=""+palabra.charAt(i);
+            a++;
+        }
+    }
     public static void verTablero(String tablero[][]){
         for(int i=0;i<tablero.length;i++){
             for(int j=0;j<tablero.length;j++){
@@ -90,19 +117,22 @@ public class Sopa1 {
     }
  
   public static void main(String[] args){
-      String[] palabras={"LETRAS","SOPA","SOL","SAL","SALON","SUECO"};
-      String[] palabrasaux={"SOPA","SOL","SAL","SALON"};
+      String palabrasHN="LETRAS";
+      String[] palabrasVN={"LETRAS"};
+      String[] palabrasDIA={"LETRAS"};
+      String[] palabrasHI={"LETRAS"};
       String tablero[][]=new String[15][15];
       Random auxf=new Random();
       Random auxc=new Random();
-      for(int i=0;i<6;i++){
-          String palabra=palabras[i];
+      /*for(int i=0;i<6;i++){
+          String palabra=palabrasHN[i];
           horizontalNormal(tablero,palabra);
-      }
-      for(int i=0;i<4;i++){
+      }*/
+      diagonalDerAbajo(tablero,palabrasHN);
+      /*for(int i=0;i<4;i++){
           String palabra=palabrasaux[i];
           verticalNormal(tablero,palabra);
-      }
+      }*/
       //verticalNormal(tablero,palabra,0,0);
       //horizontalInvertida(tablero,palabra,0,5);
       //diagonalIzqAbajo(tablero,palabra,2,2);
