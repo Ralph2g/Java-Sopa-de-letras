5050355647c125554dbcfdfe02a7c02ad91208d9
Rafael Garcia Garcia <37715000+Ralph2g@users.noreply.github.com>
2020-03-09 12:13:55 -0600

    Se llenan las palabras del juego

--- src/main/java/Servidor/Servidor.java
+++ src/main/java/Servidor/Servidor.java
@@ -22,9 +22,20 @@ public class Servidor{
     public void initList(){
     
         this.palabras = new ArrayList<>();
-        this.palabras.add(new Palabra("hola1","Concepto1",0));
-        this.palabras.add(new Palabra("Hola2","Concepto2",0));
-        this.palabras.add(new Palabra("hola1","Concepto1",0));
+        this.palabras.add(new Palabra("Computadora","Máquina electrónica que, mediante determinados programas, permite almacenar y tratar información, y resolver problemas de diversa índole.",0));
+        this.palabras.add(new Palabra("Pais","erritorio, con características geográficas y culturales propias, que puede constituir una entidad política dentro de un Estado",0));
+        this.palabras.add(new Palabra("Laboratorio","ugar dotado de los medios necesarios para realizar investigaciones, experimentos y trabajos de carácter científico o técnico.",0));
+        this.palabras.add(new Palabra("Libro","Conjunto de muchas hojas de papel u otro material semejante que, encuadernadas, forman un volumen.",0));
+        this.palabras.add(new Palabra("Tienda","Casa, puesto o lugar donde se venden al público artículos de comercio al por menor.",0));
+        this.palabras.add(new Palabra("Llave","nstrumento, comúnmente metálico, que, introducido en una cerradura, permite activar el mecanismo que la abre y la cierra.",0));
+        this.palabras.add(new Palabra("Mochila","olsa de lona o de otro material resistente que, provista de correas para ser cargada a la espalda, sirve para llevar provisiones o equipos en excursiones, expediciones, viajes",0));
+        this.palabras.add(new Palabra("Espejo","olsa de lona o de otro material resistente que, provista de correas para ser cargada a la espalda, sirve para llevar provisiones o equipos en excursiones, expediciones, viajes",0));
+        this.palabras.add(new Palabra("Colina","Elevación natural de terreno, menor que una montaña.",0));
+        this.palabras.add(new Palabra("Huracan","Viento muy impetuoso y temible que, a modo de torbellino, gira en grandes círculos, cuyo diámetro crece a medida que avanza apartándose de las zonas de calma tropicales, donde suele tener origen.",0));
+        this.palabras.add(new Palabra("Sociedad","Conjunto de personas, pueblos o naciones que conviven bajo normas comunes.",0));
+        this.palabras.add(new Palabra("Cultura","Conjunto de modos de vida y costumbres, conocimientos y grado de desarrollo artístico, científico, industrial, en una época, grupo social, etc.",0));
+        this.palabras.add(new Palabra("Empresa","Unidad de organización dedicada a actividades industriales, mercantiles o de prestación de servicios con fines lucrativos.",0));
+        this.palabras.add(new Palabra("Ciudad","Conjunto de edificios y calles, regidos por un ayuntamiento, cuya población densa y numerosa se dedica por lo común a actividades no agrícolas.",0));
         
     }
     public void startToListen()throws IOException{
